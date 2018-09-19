c2085e310dab65f62c46bfefb5f7b7e72411ab0b
Ryan Grimm <ryan@ryangrimm.com>
2018-09-19 08:02:23 -0700

    Setting the mod date and creation date to be timezone aware (UTC).

--- pdfparser/utils.py
+++ pdfparser/utils.py
@@ -1,8 +1,12 @@
 from datetime import datetime
+import pytz
 
 def parse_datestring(datestr):
     """coverts date strings in pdf metadata like `u'D:19861200000000'` to
     date objects"""
-    return datetime.strptime(datestr[2:15], '%Y%m%d%H%M%S')
+    date = datetime.strptime(datestr[2:15], '%Y%m%d%H%M%S')
+    timezone = pytz.utc
+    date = timezone.localize(date)
+    return date
 
 __all__ = [parse_datestring]
